5c978c71b1e80c20bfe7f147135e02bf3436728f
ShradhaNbr <85601322+ShradhaNbr@users.noreply.github.com>
2021-07-06 17:40:15 +0530

    Update Fibonacci.java

--- Fibonnaci/src/Fibonacci.java
+++ Fibonnaci/src/Fibonacci.java
@@ -1,17 +1,19 @@
 import java.util.Scanner;
 
-public class Fibonacci {
+public class perfectNumber {
 	public static void main(String[] args) {
-		int n, num1 = 0, num2 = 1, fib;
-		System.out.println("Enter the value");
+		int n, sum = 0;
+		System.out.println("Enter the number");
 		Scanner sc = new Scanner(System.in);
 		n = sc.nextInt();
-		sc.close();
-		for (int i = 0; i < n; i++) {
-			fib = num1 + num2;
-			num1 = num2;
-			num2 = fib;
-			System.out.println("Fibonacci series are " + fib);
+		for (int i = 1; i <= n / 2; i++) {
+			if (n % i == 0)
+				sum = sum + i;
 		}
+			if (sum == n)
+				System.out.println(n + " is a perfect number");
+			else
+				System.out.println(n + " is not a perfect number");
+
 	}
 }
